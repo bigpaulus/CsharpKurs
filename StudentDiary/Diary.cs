8cc121753945311ef65dfde6535ffb19fce294ad
bigpaulus <paulsarnecki@gmail.com>
2019-12-12 23:58:20 +0100

    Klasa i nowy obiekt. Dzienniczek, lista i funkcjonalność min/max/AVG.

--- StudentDiary/Diary.cs
+++ StudentDiary/Diary.cs
@@ -7,6 +7,34 @@ namespace StudentDiary
 {
     class Diary
     {
+        //Stan (zmienne - pola)
+        List<float> ratings = new List<float>();
 
+        //Zachowania klasy
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum = sum + rating;
+            }
+
+            avg = sum / ratings.Count();
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
     }
 }
--- StudentDiary/Program.cs
+++ StudentDiary/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace StudentDiary
 {
@@ -10,6 +9,47 @@ namespace StudentDiary
     {
         static void Main(string[] args)
         {
+            Diary diary = new Diary();
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
+
+            //float avg = diary.CalculateAverage();
+            //float max = diary.GiveMaxRating();
+            //float min = diary.GiveMinRating();
+
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocenę z zakresu 1-10");
+
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+
+                
+
+                if (rating == 11)
+                {
+                    break;
+                }
+                if (result == true)
+                {
+                    if (rating >0 && rating<=10)
+                        {
+                        diary.AddRating(rating);
+                        }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna  liczba, podaj liczbę z zakresu 1-10");
+                    }
+                    
+                }
+                
+            }
+            Console.WriteLine("Średnia Twoich ocen to: " + diary.CalculateAverage());
+            Console.WriteLine("Najwyższa ocena to:" + diary.GiveMaxRating());
+            Console.WriteLine("Najniższa ocena to:" + diary.GiveMinRating());
+
+            Console.ReadKey();
         }
     }
 }
