8b95cbd10aeb80eed5e69d5aa4d8986b96556c9f
bigpaulus <paulsarnecki@gmail.com>
2019-12-17 10:07:23 +0100

    Ćwiczenie dotyczące hermetyzacji i modyfikatorów dostępu.

---
+++ Odcinek13/DiaryStatistics.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Odcinek13
+
+{
+    public class DiaryStatistics
+    {
+        public float AverageGrade;
+        public float MaxGrade;
+        public float MinGrade;
+    }
+}
---
+++ Odcinek13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Diary diary = new Diary();
+            diary.AddRating(3f);
+            diary.AddRating(5f);
+            diary.AddRating(8.4f);
+
+            DiaryStatistics stats = diary.Computerstatistics();
+           
+            
+        }
+    }
+}
