666ee50e54786abcf0d54af0c967f3e9cf4baf7f
bigpaulus <paulsarnecki@gmail.com>
2019-12-18 22:25:35 +0100

    Immutable

---
+++ Immutable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immutable
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Immutable();
+        }
+
+        private static void Immutable()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            string name = "     Marcin     ";
+            
+            name = name.Trim();
+            Console.WriteLine(name);
+
+            Console.ReadKey();
+        }
+
+    }
+}
