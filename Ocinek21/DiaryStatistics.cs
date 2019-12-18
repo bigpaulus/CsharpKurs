1785d61e86ed2173fe20dcc6a39c0864ea3fea94
bigpaulus <paulsarnecki@gmail.com>
2019-12-18 21:21:12 +0100

    Ćwiczenia immutable.

---
+++ Ocinek21/DiaryStatistics.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Odcinek21
+
+{
+    public class DiaryStatistics
+    {
+        public float AverageGrade;
+        public float MaxGrade;
+        public float MinGrade;
+    }
+}
---
+++ Ocinek21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek21
+{
+    class Program
+    {
+        static void Givename(ref Diary diary)
+        {
+            diary = new Diary();
+            diary.Name = "Nowy Dzienniczek Marcina";
+        }
+
+        static void IncrementNumber(ref int number)
+        {
+            number += 42;
+        }
+        static void Main(string[] args)
+        {
+
+            //PassByValueAndRef();
+
+            Immutable();
+
+        }
+
+        private static void Immutable()
+        {
+
+            string name = " Marcin ";
+            name = name.Trim();
+
+            DateTime date = new DateTime(2015, 3, 10);
+            date = date.AddHours(48);
+
+
+            Console.ReadKey();
+        }
+     
+        
+    }
+}
