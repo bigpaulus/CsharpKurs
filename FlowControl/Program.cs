60078c2a13e9df8551e04ac947a6e08411b7153b
bigpaulus <paulsarnecki@gmail.com>
2019-12-21 18:17:17 +0100

    Używanie pętli for\foreach \while \do while.

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -14,53 +14,136 @@ namespace FlowControl
 
         static void Main(string[] args)
         {
+            //InsructioIf();
+
+            //InstructionSwitch();
+
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int a = 0; a < 2; a++)
+            //    {
+            //        Console.WriteLine("wartość i : {0} oraz wartość a : {1}", i,a);
+            //    }
+
+            //}
+
+            int i = 1;
+            //while (i > 3)
+
+            //{
+            //    Console.WriteLine("Wartość i {0}", i);
+            //    i++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("Wartość i {0}", i);
+            //   i++;
+
+            //} while (i>3);
+
+            //int[] tab = { 1, 2, 3, 5, 8, 12, 21 };
+            //foreach (var item in tab)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            for (; ; )
+            {
+                Console.WriteLine("Podaj Imię:");
+                string text = Console.ReadLine();
+                Console.WriteLine("Masz na imię {0} ", text);
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+
+        private static void InstructionSwitch()
+        {
+            Console.WriteLine("Podaj Imię: ");
+            name = Console.ReadLine();
+
+
+            switch (name)
+            {
+                case "Marcin":
+                    Method1();
+                    break;
+                case "Tomek":
+                    Method2();
+                    Method3();
+                    break;
+                case "Ania":
+                    Method1();
+                    Method2();
+                    Method3();
+
+                    break;
+
+                default:
+                    Method4();
+                    break;
+            }
+        }
+
+        private static void InsructioIf()
+        {
             Console.WriteLine("Podaj swój wiek: ");
             age = int.Parse(Console.ReadLine());
 
             string test = age >= 18 ? "Jesteś pełnoletni" : "Jesteś dzieckiem";
             Console.WriteLine(test);
-           
+
             Console.ReadKey();
 
-            //Console.WriteLine("Podaj swoje imię: ");
-            //name = Console.ReadLine();
+            Console.WriteLine("Podaj swoje imię: ");
+            name = Console.ReadLine();
 
-            //if (age <= 10)
+            if (age <= 10)
 
-            //{
-            //    if (name == "Marcin")
-            //    {
-            //        Method1();
-            //    }
-            //}
-            //else if (age < 20)
-            //{
-            //    Method2();
-            //}
-            //else if (age < 30)
-            //{
-            //    Method3();
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Masz 30 lub więcej lat");
-            //}
-            //Console.ReadKey();
+            {
+                if (name == "Marcin")
+                {
+                    Method1();
+                }
+            }
+            else if (age < 20)
+            {
+                Method2();
+            }
+            else if (age < 30)
+            {
+                Method3();
+            }
+            else
+            {
+                Console.WriteLine("Masz 30 lub więcej lat");
+            }
+            Console.ReadKey();
+        }
+
+        private static void Method4()
+        {
+            Console.WriteLine("jesteś w metodzie 4");
         }
 
         private static void Method3()
         {
-            Console.WriteLine("Twój wiek jest większy lub równy 30 i wynosi: " + age);
+            Console.WriteLine("jesteś w metodzie 3");
         }
 
         private static void Method2()
         {
-            Console.WriteLine("Twój wiek jest większy niż 10 lecz mniejszy niż 20 i wynosi: " +age);
+            Console.WriteLine("jesteś w metodzie 2");
         }
 
         private static void Method1()
         {
-            Console.WriteLine("Masz 10 lub mniej lat, dokładnie masz: " + age);
+            Console.WriteLine("jesteś w metodzie 1");
         }
         
     }
