930f7c4cf915852e1538550a8c543d4b73f4650f
bigpaulus <paulsarnecki@gmail.com>
2019-12-21 23:08:54 +0100

    Użycie Finally i Using.

--- Members/Program.cs
+++ Members/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,70 +13,94 @@ namespace Members
     {
         static void Main(string[] args)
         {
-            Diary diary = new Diary();
+            #region
+            //Diary diary = new Diary();
 
 
+            //try
+            //{
+
+            //    Console.WriteLine("Prosze podać imię: ");
+            //    diary.Name = Console.ReadLine();
+            //}
+
+            //catch (ArgumentException ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (NullReferenceException)
+            //{
+
+            //    Console.WriteLine("Coś poszło nie tak..");
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+
+            //diary.NameChanged += OnNameChanged;
+            //diary.NameChanged += OnNameChanged2;
+            //diary.NameChanged += OnNameChanged3;
+            //diary.NameChanged += OnNameChanged4;
+            //diary.NameChanged += OnNameChanged4;
+            //diary.NameChanged += OnNameChanged4;
+            //diary.NameChanged += OnNameChanged4;
+            //diary.NameChanged -= OnNameChanged4;
+            //diary.NameChanged -= OnNameChanged4;
+            //diary.NameChanged -= OnNameChanged4;
+
+
+
+
+            //    diary.Name = "Dzienniczek Marcina";
+            //    diary.Name = "Jacek Hej";
+
+            //    Console.WriteLine(diary.Name);
+
+            //}
+
+            //private static void OnNameChanged(object sender, NameChangedEventArgs args)
+            //{
+            //    Console.WriteLine($"Zmiana nazwy z {args.EgsistingName} na {args.NewName}");
+            //}
+            //private static void OnNameChanged2(object sender, NameChangedEventArgs args)
+            //{
+            //    Console.WriteLine("*****************");
+            //}
+            //private static void OnNameChanged3(object sender, NameChangedEventArgs args)
+            //{
+            //    Console.WriteLine("Hello!");
+            //}
+            //private static void OnNameChanged4(object sender, NameChangedEventArgs args)
+            //{
+            //    Console.WriteLine("*****************");
+            #endregion stary kod
+
+            StreamWriter file = new StreamWriter("C:\\Katalog\\plik.txt",false);
             try
             {
+                Console.WriteLine("Podaj swoje imie:");
+                string name = Console.ReadLine();
 
-                Console.WriteLine("Prosze podać imię: ");
-                diary.Name = Console.ReadLine();
+                file.WriteLine(name);
+                file.WriteLine("Witaj piszemy do pliku tekstowego");
             }
-            
-            catch (ArgumentException ex)
+            finally
             {
-
-                Console.WriteLine(ex.Message);
+                file.Close();
             }
-            catch (NullReferenceException)
+
+            using(StreamWriter file2 = new StreamWriter("C:\\Katalog\\plik.txt", true))
             {
-
-                Console.WriteLine("Coś poszło nie tak..");
-            }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine(ex.Message);
+                file2.WriteLine("Witaj piszemy do pliku tekstowego!!!!!!!!!!!!!");
+               
             }
 
-
-
-            diary.NameChanged += OnNameChanged;
-            diary.NameChanged += OnNameChanged2;
-            diary.NameChanged += OnNameChanged3;
-            diary.NameChanged += OnNameChanged4;
-            diary.NameChanged += OnNameChanged4;
-            diary.NameChanged += OnNameChanged4;
-            diary.NameChanged += OnNameChanged4;
-            diary.NameChanged -= OnNameChanged4;
-            diary.NameChanged -= OnNameChanged4;
-            diary.NameChanged -= OnNameChanged4;
-
-          
-
-
-            diary.Name = "Dzienniczek Marcina";
-            diary.Name = "Jacek Hej";
-
-            Console.WriteLine(diary.Name);
-
         }
 
-        private static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Zmiana nazwy z {args.EgsistingName} na {args.NewName}");
-        }
-        private static void OnNameChanged2(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine("*****************");
-        }
-        private static void OnNameChanged3(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine("Hello!");
-        }
-        private static void OnNameChanged4(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine("*****************");
-        }
     }
 }
